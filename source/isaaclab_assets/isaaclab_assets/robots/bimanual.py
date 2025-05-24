5901ceac3da6752c7143f78975b0074ac3543bb5
Tyler Lum <tylergwlum@gmail.com>
2025-05-23 17:20:11 -0700

    No collision for blue

--- source/isaaclab_assets/isaaclab_assets/robots/bimanual.py
+++ source/isaaclab_assets/isaaclab_assets/robots/bimanual.py
@@ -45,7 +45,7 @@ GREEN_RGB = (0.0, 1.0, 0.0)
 
 BIMANUAL_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/bimanual_kuka_allegro_v9/usd/bimanual_kuka_allegro.usd",
+        usd_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/bimanual_kuka_allegro_v12/usd/bimanual_kuka_allegro.usd",
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=True,
@@ -216,6 +216,9 @@ BIMANUAL_CFG = ArticulationCfg(
 )
 
 BLUE_BIMANUAL_CFG = BIMANUAL_CFG.copy()
-BLUE_BIMANUAL_CFG.spawn.usd_path = f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/bimanual_kuka_allegro_v9/usd_blue/bimanual_kuka_allegro.usd"
+BLUE_BIMANUAL_CFG.spawn.usd_path = f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/bimanual_kuka_allegro_v12/usd_blue/bimanual_kuka_allegro.usd"
+BLUE_BIMANUAL_CFG.spawn.collision_props = sim_utils.CollisionPropertiesCfg(
+    collision_enabled=False,
+)  # No collision
 
 """Configuration for the Bimanual robot consisting of Kuka arms and Allegro hands."""
