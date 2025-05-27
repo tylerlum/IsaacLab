75eea6f24f646d7c8b5104ce93dac9b834de7d2d
Tyler Lum <tylergwlum@gmail.com>
2025-05-26 18:53:31 -0700

    Adjust table position

--- source/isaaclab_tasks/isaaclab_tasks/direct/tyler/bimanual/utils/table_constants.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/tyler/bimanual/utils/table_constants.py
@@ -9,11 +9,18 @@ TABLE_LENGTH_X, TABLE_LENGTH_Y, TABLE_LENGTH_Z = (
 
 # TC is table center
 X_R_TC = np.eye(4)
+# X_R_TC[:3, 3] = np.array(
+#     [
+#         TABLE_LENGTH_X / 2,
+#         0,
+#         0,
+#     ]
+# )
 X_R_TC[:3, 3] = np.array(
     [
-        TABLE_LENGTH_X / 2,
+        TABLE_LENGTH_X / 2 + 0.3,
         0,
-        0,
+        0.3,
     ]
 )
 
