8334571c63d612d6723b963c79b9808a7a0a9f46
Tyler Lum <tylergwlum@gmail.com>
2025-05-26 17:23:29 -0700

    Tune rsl rl params

--- source/isaaclab_tasks/isaaclab_tasks/direct/tyler/bimanual/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/tyler/bimanual/agents/rsl_rl_ppo_cfg.py
@@ -13,25 +13,29 @@ from isaaclab_rl.rsl_rl import (
 
 @configclass
 class BimanualPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    normalize_advantage_per_mini_batch = True
     num_steps_per_env = 24
     max_iterations = 3000
     save_interval = 50
     experiment_name = "bimanual"
-    empirical_normalization = False
+    empirical_normalization = True
+    logger = "wandb"
+    wandb_project = "isaaclab"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
+        actor_hidden_dims=[512, 512, 512],
+        critic_hidden_dims=[512, 512, 512],
         activation="elu",
+        noise_std_type="scalar",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.008,
+        entropy_coef=0.01,
         num_learning_epochs=5,
-        num_mini_batches=4,
-        learning_rate=1.0e-3,
+        num_mini_batches=16,  # mini batch size = num_envs * num_steps / num_mini_batches
+        learning_rate=3e-4,
         schedule="adaptive",
         gamma=0.99,
         lam=0.95,
